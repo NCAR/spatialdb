8e5ef7e02c64e5bd8085e5e3fc21bbc4baafa0df
Charlie Martin <martinc@ucar.edu>
2011-04-28 21:55:35 +0000

    Removed all of the spatialite libraries that I could find. Then followed spatialite instructions for building and installing the libspatialite-amalgamation under mingw. Then removed all specialized specifications for include and library directories. Project builds now and mmtest runs fine. It does still use the libspatialite-2.dll. Static linking seems to be a challenge.

--- tool_spatialdb.py
+++ tool_spatialdb.py
@@ -5,7 +5,6 @@ import eol_scons
 tools = ['sqlitedb','doxygen','prefixoptions']
 env = Environment(tools = ['default'] + tools)
 platform = env['PLATFORM']
-#if platform == 'win32':
 env.AppendUnique(CPPDEFINES=['SPATIALITE_AMALGAMATION',])
 
 libsources = Split("""
@@ -16,7 +15,7 @@ headers = Split("""
 SpatiaLiteDB.h
 """)
 
-libspatialdb = env.Library('spatialdb', libsources + ['/opt/local/lib/libspatialite.a'])
+libspatialdb = env.Library('spatialdb', libsources)
 
 html = env.Apidocs(libsources + headers,  DOXYFILE_DICT={'PROJECT_NAME':'SpatiaLiteDB', 'PROJECT_NUMBER':'1.0'})
 env.Default(html)
@@ -26,11 +25,8 @@ env.Default(libspatialdb)
 thisdir = env.Dir('.').srcnode().abspath
 
 def spatialdb(env):
-    env.AppendUnique(CPPPATH=['/opt/local/include',]) 
-    env.AppendUnique(LIBPATH=['/opt/local/lib',])
     env.AppendUnique(CPPPATH   =[thisdir,])
     env.AppendLibrary('spatialdb')
-    env.AppendLibrary('spatialite')
     env.AppendLibrary('geos')
     env.AppendLibrary('geos_c')
     env.AppendLibrary('proj')
@@ -38,7 +34,6 @@ def spatialdb(env):
 
     env.AppendDoxref('SpatialDB')
     
-    #if platform == 'win32':
     env.AppendUnique(CPPDEFINES=['SPATIALITE_AMALGAMATION',])
 
     env.Require(tools)
