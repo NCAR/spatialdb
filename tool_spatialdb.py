384a3b4ca36b6be50a3d7c6febb0b669898f5535
Charlie Martin <martinc@ucar.edu>
2011-05-13 16:15:13 +0000

    Add some comments and clean up the tool code.

--- tool_spatialdb.py
+++ tool_spatialdb.py
@@ -1,27 +1,24 @@
+# SpatialDB scons tool
+#
+# It builds the library for the SpatialDB C++ class library,
+# and provides CPP and linker specifications for the header 
+# and libraries.
+#
+# SpatialDB depends on SqliteDB, which provides the interface to 
+# sqlite3. It also depends on SpatiaLite. Since SpatiaLite is 
+# is also needed by SQLiteDB, we let the sqlitedb tool provide
+# the required SpatiaLite plumbing.
+
 import os
 import sys
-
 import eol_scons
+
 tools = ['sqlitedb','doxygen','prefixoptions']
 env = Environment(tools = ['default'] + tools)
 platform = env['PLATFORM']
 thisdir = env.Dir('.').srcnode().abspath
 
-libsources = Split("""
-SpatiaLiteDB.cpp
-""")
-
-headers = Split("""
-SpatiaLiteDB.h
-""")
-
-env.AppendUnique(CPPDEFINES=['SPATIALITE_AMALGAMATION',])
-libspatialdb = env.Library('spatialdb', libsources)
-env.Default(libspatialdb)
-
-html = env.Apidocs(libsources + headers,  DOXYFILE_DICT={'PROJECT_NAME':'SpatiaLiteDB', 'PROJECT_NUMBER':'1.0'})
-env.Default(html)
-
+# define the tool
 def spatialdb(env):
     env.AppendUnique(CPPPATH   =[thisdir,])
     env.AppendLibrary('spatialdb')
@@ -30,10 +27,24 @@ def spatialdb(env):
     env.AppendLibrary('proj')
     if (platform != 'posix'):
         env.AppendLibrary('iconv')
-    env.AppendDoxref('SpatialDB')  
-    env.AppendUnique(CPPDEFINES=['SPATIALITE_AMALGAMATION',])
     env.Replace(CCFLAGS=['-g','-O2'])
     env.Require(tools)
 
 Export('spatialdb')
+
+# build the SpatialDB library
+libsources = Split("""
+SpatiaLiteDB.cpp
+""")
+
+headers = Split("""
+SpatiaLiteDB.h
+""")
+
+libspatialdb = env.Library('spatialdb', libsources)
+env.Default(libspatialdb)
+
+html = env.Apidocs(libsources + headers,  DOXYFILE_DICT={'PROJECT_NAME':'SpatiaLiteDB', 'PROJECT_NUMBER':'1.0'})
+env.Default(html)
+
 
